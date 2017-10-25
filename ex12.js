8ad6cee9dd70360c23bfe3ebd48b99bfeae4ac07
Patrick White <byelipk@kso2.local>
2017-10-25 17:29:47 -0400

    Custom pipe and compose

---
+++ ex12.js
@@ -0,0 +1,23 @@
+function compose(...fns) {
+  return pipe(...fns.reverse());
+}
+
+function pipe(...fns) {
+  return function piped(result) {
+    fns.forEach(function loop(fn) {
+      result = fn(result);
+    });
+    return result;
+  }
+}
+
+function increment(x) { return x + 1; }
+function decrement(x) { return x - 1; }
+function double(x) { return x * 2; }
+function half(x) { return x / 2; }
+
+var f = compose(decrement, double, increment, half);
+var p = pipe(half, increment, double, decrement);
+
+console.log( f(3) )
+console.log( p(3) )
