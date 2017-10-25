0975ebdb6aa3abb7d47e663495d6427c4103b70b
Patrick White <byelipk@kso2.local>
2017-10-25 18:40:59 -0400

    Added notes about immutability

---
+++ ex13.js
@@ -0,0 +1,8 @@
+// What is immutability?
+//
+// What about the `const` keyword?
+//
+// The const keyword is about assignment immutability. When we use const
+// we mean a variable cannot be reassigned.
+//
+// Assignment immutability vs assignment immutability?
