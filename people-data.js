fd7a263d540a3c304422e126a218e55d420fe7a0
Patrick White <byelipk@kso2.local>
2017-11-03 10:54:01 -0400

    More boiler plate

--- people-data.js
+++ people-data.js
@@ -7,20 +7,13 @@
 //   "countries": [
 //     {
 //       "count": 1,
-//       "people": ["attendee@acmepartners.com"],
+//       "people": ["attendee@acme.com"],
 //       "name": "Wales",
 //       "start": "2017-04-29"
 //     },
 //     ...
 //   ]
 // }
-//
-// Here's the algorithm for the start date for each country:
-//
-//  1. Select the two-day period where the most people can attend for 
-//     both days in a row.
-//  2. If there are multiple dates, choose the earlier date.
-//  3. If there are no two-day periods, start date should be set to null.
 
 var { 
   compose, 
@@ -39,70 +32,70 @@ const DATA = Object.freeze({
     {
       "first": "Robby",
       "last": "Dee",
-      "email": "robbydee@acmepartners.com",
+      "email": "robbydee@acme.com",
       "country": "United States",
       "dates": ["2017-05-03", "2017-05-06"]
     },
     {
       "first": "Bee",
       "last": "Queen",
-      "email": "beequeen@acmepartners.com",
+      "email": "beequeen@acme.com",
       "country": "Ireland",
       "dates": ["2017-04-27", "2017-04-29", "2017-04-30"]
     },
     {
       "first": "Jane",
       "last": "Google",
-      "email": "janegoogle@acmepartners.com",
+      "email": "janegoogle@acme.com",
       "country": "Spain",
       "dates": ["2017-04-29", "2017-04-30", "2017-05-01"]
     },
     {
       "first": "Tiff",
       "last": "Lukas",
-      "email": "tifflukas@acmepartners.com",
+      "email": "tifflukas@acme.com",
       "country": "Spain",
       "dates": ["2017-04-28", "2017-04-29", "2017-05-01", "2017-05-04"]
     },
     {
       "first": "Sherlock",
       "last": "Temple",
-      "email": "sherlocktemple@acmepartners.com",
+      "email": "sherlocktemple@acme.com",
       "country": "Spain",
       "dates": ["2017-04-28", "2017-04-29", "2017-05-02", "2017-05-04"]
     },
     {
       "first": "Robin",
       "last": "Yearwood",
-      "email": "robinyearwood@acmepartners.com",
+      "email": "robinyearwood@acme.com",
       "country": "Spain",
       "dates": ["2017-04-29", "2017-04-30", "2017-05-02", "2017-05-03"]
     },
     {
       "first": "Shirly",
       "last": "Temple",
-      "email": "stemple@acmepartners.com",
+      "email": "stemple@acme.com",
       "country": "Spain",
       "dates": ["2017-04-30", "2017-05-01"]
     },
     {
       "first": "Lovejoy",
       "last": "Magic",
-      "email": "lmagic@acmepartners.com",
+      "email": "lmagic@acme.com",
       "country": "Spain",
       "dates": ["2017-04-28", "2017-04-29", "2017-05-01", "2017-05-03"]
     },
     {
       "first": "Mister",
       "last": "Wilbur",
-      "email": "mwilbur@acmepartners.com",
+      "email": "mwilbur@acme.com",
       "country": "Spain",
       "dates": ["2017-04-29", "2017-04-30", "2017-05-02", "2017-05-03"]
     },
     {
       "first": "Sir",
       "last": "Newton",
-      "email": "snewton@acmepartners.com",
+      "email": "snewton@acme.com",
       "country": "United States",
       "dates": ["2017-05-04", "2017-05-09"]
     }
